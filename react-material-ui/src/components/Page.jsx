33e5a84db1d2781abbfdc0e7bfeb2b007755574b
EzequielVillalba112 <148280303+EzequielVillalba112@users.noreply.github.com>
2023-11-14 18:06:01 -0300

    q

--- react-material-ui/src/components/Page.jsx
+++ react-material-ui/src/components/Page.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import ContainerSx from './ContainerSx/ContainerSx'
 import Typography from './Typography/Typography'
-import App from '../App'
-import Header from './Header/Header'
-
 
 export default function Page() {
   return (
     <>
         <Routes>
-            <Route path='/' element={<App/>}/>
+            <Route path='/' element={<ContainerSx/>}/>
             <Route path='/container-sx' element={<ContainerSx/>}/>
             <Route path='/typografy' element={<Typography/>}/>
         </Routes>
